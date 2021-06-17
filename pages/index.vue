f6097a3536dad198bd88e9deada7f654258bcc63
Ben Hong <ben@bencodezen.io>
2021-06-17 13:01:40 -0400

    refactor: use a component

--- pages/index.vue
+++ pages/index.vue
@@ -5,35 +5,19 @@
       <h1 class="title">
         simple-nuxt-form-demo
       </h1>
-      <form name="contact" method="POST" data-netlify="true">
-        <p>
-          <label>Your Name: <input type="text" name="name"/></label>
-        </p>
-        <p>
-          <label>Your Email: <input type="email" name="email"/></label>
-        </p>
-        <p>
-          <label
-            >Your Role:
-            <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select></label
-          >
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
+      <ContactForm />
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import ContactForm from "../components/ContactForm.vue";
+
+export default {
+  components: {
+    ContactForm
+  }
+};
 </script>
 
 <style>
